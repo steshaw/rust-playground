b371b033ba10719408523946ab3473614ce8a8d9
Steven Shaw <steven@steshaw.org>
2017-07-02 10:02:46 +1000

    Initial

---
+++ leap/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn is_leap_year(year: i16) -> bool {
+  /*
+   * on every year that is evenly divisible by 4
+   *   except every year that is evenly divisible by 100
+   *     unless the year is also evenly divisible by 400
+   */
+  year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+}
