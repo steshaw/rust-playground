bfc6c36af5d8a947e5e26a029d2a59213dc4baf8
Steven Shaw <steven@steshaw.org>
2019-12-08 00:23:12 +1000

    assert_eq

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -4,6 +4,7 @@ use num_format::{Locale, ToFormattedString};
 async fn hello() {
     let answer = 1_234_5;
     println!("Hello, {}", answer.to_formatted_string(&Locale::en));
+    assert_eq!(answer, 12_345);
 }
 
 #[async_std::main]
