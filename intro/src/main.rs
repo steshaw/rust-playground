b01bb4ee81bc6a98407e6c7fe60d05cdb77e80c7
Steven Shaw <steven@steshaw.org>
2019-12-08 00:37:13 +1000

    mut

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -12,10 +12,23 @@ async fn hello(even_odd: &str, i: i32) {
     assert_eq!(answer, 12_345);
 }
 
-#[async_std::main]
-async fn main() {
+async fn hellos() {
     for i in 1..=3 {
         let even_odd = if i % 2 == 0 { "even" } else { "odd" };
         hello(even_odd, i).await;
     }
+}
+
+fn sum() {
+    let mut sum = 0;
+    for i in 0..5 {
+        sum += i;
+    }
+    println!("sum is {}", sum);
+}
+
+#[async_std::main]
+async fn main() {
+    hellos().await;
+    sum();
 }
