d879ec98a75783eb65f2810910b7d28d04224528
Steven Shaw <steven@steshaw.org>
2019-12-08 00:28:48 +1000

    for + if

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -1,13 +1,19 @@
 use async_std;
 use num_format::{Locale, ToFormattedString};
 
-async fn hello() {
+async fn hello(even_odd : &str, i : i32) {
     let answer = 1_234_5;
-    println!("Hello, {}", answer.to_formatted_string(&Locale::en));
+    println!("{}: Hello, {} {}", i, even_odd, answer.to_formatted_string(&Locale::en));
     assert_eq!(answer, 12_345);
 }
 
 #[async_std::main]
 async fn main() {
-    hello().await;
+    for i in 1..=3 {
+      if  i % 2 == 0 {
+        hello("even", i).await;
+      } else {
+        hello("odd", i).await;
+      }
+    }
 }
