aa5318fb86fecf94ce341a53b01a430b76f40a99
Steven Shaw <steven@steshaw.org>
2019-12-08 00:20:04 +1000

    Format with num_format

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -1,8 +1,9 @@
 use async_std;
+use num_format::{Locale, ToFormattedString};
 
 async fn hello() {
     let answer = 1_234_5;
-    println!("Hello, {}", answer);
+    println!("Hello, {}", answer.to_formatted_string(&Locale::en));
 }
 
 #[async_std::main]
