47c959962731083bfba434ef585bcd0fc7de15fc
Steven Shaw <steven@steshaw.org>
2019-12-08 00:32:58 +1000

    rustfmt

--- intro/src/main.rs
+++ intro/src/main.rs
@@ -1,19 +1,21 @@
 use async_std;
 use num_format::{Locale, ToFormattedString};
 
-async fn hello(even_odd : &str, i : i32) {
+async fn hello(even_odd: &str, i: i32) {
     let answer = 1_234_5;
-    println!("{}: Hello, {} {}", i, even_odd, answer.to_formatted_string(&Locale::en));
+    println!(
+        "{}: Hello, {} {}",
+        i,
+        even_odd,
+        answer.to_formatted_string(&Locale::en)
+    );
     assert_eq!(answer, 12_345);
 }
 
 #[async_std::main]
 async fn main() {
     for i in 1..=3 {
-      if  i % 2 == 0 {
-        hello("even", i).await;
-      } else {
-        hello("odd", i).await;
-      }
+        let even_odd = if i % 2 == 0 { "even" } else { "odd" };
+        hello(even_odd, i).await;
     }
 }
