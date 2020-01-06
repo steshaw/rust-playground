c3b6116a2a69cc769ca4f19619a3c20b8399500c
Steven Shaw <steven@steshaw.org>
2020-01-06 21:47:31 +1000

    More FizzBuzz

--- crash/examples/01-kick-fizzbuzz.rs
+++ crash/examples/01-kick-fizzbuzz.rs
@@ -34,23 +34,73 @@ fn fizzbuzz3(i: u8) {
     println!("{}", fb(i));
 }
 
+fn fizzbuzz4() {
+    fn fb(i: u8) -> String {
+        match (i % 3, i % 5) {
+            (0, 0) => "fizzbuzz".to_string(),
+            (0, _) => "fizz".to_string(),
+            (_, 0) => "buzz".to_string(),
+            (_, _) => format!("{}", i),
+        }
+    }
+    (1..=100).for_each(|i| println!("{}", fb(i)))
+}
+
+fn fizzbuzz5() {
+    fn fb(i: u8) -> String {
+        match (i % 3, i % 5) {
+            (0, 0) => "fizzbuzz".to_string(),
+            (0, _) => "fizz".to_string(),
+            (_, 0) => "buzz".to_string(),
+            (_, _) => format!("{}", i),
+        }
+    }
+    for s in (1..=100).map(fb) {
+        println!("{}", s);
+    }
+}
+
+fn fizzbuzz6() {
+    use std::fmt;
+    struct FizzBuzzer(u8);
+    impl fmt::Display for FizzBuzzer {
+        fn fmt(self: &Self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let FizzBuzzer(i) = self;
+            match (i % 3, i % 5) {
+                (0, 0) => write!(fmt, "fizzbuzz"),
+                (0, _) => write!(fmt, "fizz"),
+                (_, 0) => write!(fmt, "buzz"),
+                (_, _) => write!(fmt, "{}", i),
+            }
+        }
+    }
+    for fizzbuzzer in (1..=100).map(FizzBuzzer) {
+        println!("{}", fizzbuzzer);
+    }
+}
+
 fn main() {
+    fn fb(f: fn(u8) -> ()) {
+        for i in 1..=100 {
+            f(i);
+        }
+    }
     fn usage() -> ! {
         panic!("usage: fizzbuzz <num>")
     }
-    let args = std::env::args();
+    let args = std::env::args().collect::<Vec<_>>();
     if args.len() != 2 {
         usage();
     }
     #[allow(clippy::identity_conversion)]
-    let a1 = args.into_iter().nth(1);
-    let fb = match a1 {
-        Some(s) if s == "1" => fizzbuzz1,
-        Some(s) if s == "2" => fizzbuzz2,
-        Some(s) if s == "3" => fizzbuzz3,
-        _ => usage()
-    };
-    for i in 1..=100 {
-        fb(i);
+    let a1 = &args[1]; // NOTE: Indexing protect by len check.
+    match a1 {
+        s if s == "1" => fb(fizzbuzz1),
+        s if s == "2" => fb(fizzbuzz2),
+        s if s == "3" => fb(fizzbuzz3),
+        s if s == "4" => fizzbuzz4(),
+        s if s == "5" => fizzbuzz5(),
+        s if s == "6" => fizzbuzz6(),
+        _ => usage(),
     }
 }
