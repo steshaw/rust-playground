3a0f00168ce75a359e1e25b60fc73fcab67f612c
Steven Shaw <steven@steshaw.org>
2020-01-06 10:40:12 +1000

    Add intersperse to Iterator

--- crash/examples/intersperse.rs
+++ crash/examples/intersperse.rs
@@ -1,4 +1,14 @@
-// FIXME: the horrors.
+//
+// FIXME: Fix the horrors here.
+//
+// TODO: Implement intersperse over iterators.
+//
+// ghci> intersperse 0 [1,2,3]
+// [1,0,2,0,3]
+//
+// TODO: Is this `join` from the std? i.e. `xs.iter().join(0)`?
+// TODO: Seems it might be available with +nightly.
+//
 
 use std::iter::Peekable;
 
@@ -36,25 +46,29 @@ where
     }
 }
 
-fn intersperse<T, I>(x: T, xs: I) -> Intersperse<T, I>
-where
-    I: Iterator<Item = T>,
-    T: Clone,
-{
-    Intersperse {
-        inject: false,
-        t: x,
-        iter: xs.peekable(),
+//
+// Intersperse as an "extension method",
+// with help from Jake Goulding (Shepmaster):
+//
+//   https://stackoverflow.com/a/30540952/482382
+//
+trait IntersperseExt {
+    fn intersperse<T>(self, x : T) -> Intersperse<T, Self>
+    where
+        Self: Iterator<Item = T>,
+        T: Clone,
+        Self: Sized
+    {
+        Intersperse {
+            inject: false,
+            t: x,
+            iter: self.peekable(),
+        }
     }
 }
 
-//
-// TODO: Implement intersperse over iterators.
-//
-// ghci> intersperse 0 [1,2,3]
-// [1,0,2,0,3]
-//
-// TODO: Is this `join` from the std? i.e. `xs.iter().join(0)`?
+impl<I: Iterator> IntersperseExt for I {}
+
 //
 // FIXME: Implement this better :)
 // FIXME: Use a type paramater (rather than u32).
@@ -83,7 +97,7 @@ fn main() {
 
     println!("intersperse:");
     let xs = vec![1, 2, 3];
-    let ys : Vec<&i32> = intersperse(&0, xs.iter()).collect();
+    let ys : Vec<&i32> = xs.iter().intersperse(&0).collect();
     assert_eq!(vec![&1, &0, &2, &0, &3], ys);
     println!("ys = {:#?}", ys);
 }
