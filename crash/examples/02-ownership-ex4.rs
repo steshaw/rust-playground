b2f7d71aa863623a648703d397163a8c873dc025
Steven Shaw <steven@steshaw.org>
2020-01-11 21:49:20 +1000

    Try alternative solution to ex4

--- crash/examples/02-ownership-ex4.rs
+++ crash/examples/02-ownership-ex4.rs
@@ -1,12 +1,15 @@
 #[derive(Debug)]
 struct Foobar(i32);
 
-fn double(i: Foobar) -> Foobar {
-    Foobar(i.0 * 2)
+fn double(mut i: Foobar) -> Foobar {
+    i.0 *= 2;
+    i
 }
 
 fn main() {
     let x: Foobar = Foobar(1);
+    println!("x = {}", x.0);
     let y: Foobar = double(x);
-    println!("{}", y.0);
+//    println!("x = {}", x.0);
+    println!("y = {}", y.0);
 }
