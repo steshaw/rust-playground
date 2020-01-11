9b361a02d02809db29c705540fdcdd9a2f5a373f
Steven Shaw <steven@steshaw.org>
2020-01-11 16:39:08 +1000

    Switch to Rust nightly

--- .config/nix/shell.nix
+++ .config/nix/shell.nix
@@ -1,36 +1,49 @@
 let
   pinnedPkgs = import ./pkgs.nix;
-in { pkgs ? pinnedPkgs }:
-let
-  moz_overlay = import (
-    builtins.fetchTarball
-      "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz"
-  );
-  pkgs_overlay = pkgs {
-    overlays = [
-      moz_overlay
-    ];
-  };
-  #rustNightlyChannel = (nixpkgs.rustChannelOf { date = "2019-01-26"; channel = "nightly"; }).rust;
-  rustStableChannel = pkgs_overlay.latest.rustChannels.stable.rust.override {
-    extensions = [
-      "rust-src"
-      "rls-preview"
-      "clippy-preview"
-      "rustfmt-preview"
-    ];
-  };
-in with pkgs_overlay;
-stdenv.mkDerivation {
-  name = "moz_overlay_shell";
-  buildInputs = [
-    rustStableChannel
+in
+{ pkgs ? pinnedPkgs }:
+  let
+    mozOverlay = import (
+      builtins.fetchTarball
+        "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz"
+    );
+    mozPkgs = pkgs {
+      overlays = [
+        mozOverlay
+      ];
+    };
 
-    cacert
-    watchexec
+    useNightly = true;
 
-    clang
-    clang-tools
-    gnumake
-  ];
-}
+    rust = if useNightly then
+      let
+        rustChannel = mozPkgs.rustChannelOf {
+          date = "2020-01-11";
+          channel = "nightly";
+        };
+      in
+        rustChannel.rust
+    else
+      mozPkgs.latest.rustChannels.stable.rust.override {
+        extensions = [
+          "clippy-preview"
+          "rls-preview"
+          "rust-src"
+          "rustfmt-preview"
+        ];
+      };
+  in
+    with mozPkgs;
+    stdenv.mkDerivation {
+      name = "moz_overlay_shell";
+      buildInputs = [
+        rust
+
+        cacert
+        watchexec
+
+        clang
+        clang-tools
+        gnumake
+      ];
+    }
