6ab795383c00caf7d83e122602062061216e557d
Steven Shaw <steven@steshaw.org>
2020-01-12 12:57:20 +1000

    spawn

--- misc/examples/traits.rs
+++ misc/examples/traits.rs
@@ -1,5 +1,7 @@
 // https://blog.rust-lang.org/2015/05/11/traits.html
 
+use std::process::*;
+
 struct Point {
     x: f64,
     y: f64,
@@ -25,4 +27,26 @@ fn main() {
     let s2 = (&p).to_s(); // explicit borrow.
     let s3 = point_to_string(&p); // required explicit borrow.
     println!("{} {} {}", s1, s2, s3);
+
+    let child_e = Command::new("/home/steshaw/.nix-profile/bin/echo")
+        .arg("Hello,")
+        .arg("world!")
+        .current_dir(".")
+        .stdout(Stdio::piped())
+        .spawn();
+    let r = child_e
+        .and_then(|child| match child.wait_with_output() {
+            Ok(out) => {
+                println!("out = {:?}", out);
+                println!("out.stdout = {:?}", out.stdout);
+                assert_eq!(out.stdout, b"Hello, world!\n");
+                Ok(out)
+            }
+            Err(err) => Err(err),
+        })
+        .or_else(|e| {
+            println!("An error occurred: {}", e);
+            Err(e)
+        });
+    println!("r = {:?}", r); // Use r :-)
 }
