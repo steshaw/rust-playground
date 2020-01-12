345e3a6a220862fe1ab3b3f443cc1045eb2b87eb
Steven Shaw <steven@steshaw.org>
2020-01-12 16:27:13 +1000

    Inline closures

--- misc/examples/trait-callback.rs
+++ misc/examples/trait-callback.rs
@@ -20,26 +20,12 @@ impl<T: ClickCallback + Clone> Button<'_, T> {
     }
 }
 
-/*
-fn call_with_one<F>(func: F) -> usize
-where
-    F: Fn(usize) -> usize,
-{
-    func(1)
-}
-
-let double = |x| x * 2;
-assert_eq!(call_with_one(double), 2);
-*/
-
-type CB = dyn Fn(&'static str, i64, i64) -> ();
+type OnClick = dyn Fn(&'static str, i64, i64) -> ();
 
 #[derive(Clone)]
-struct FnCallback<'a>
-//where F: Fn(FnCallback, i64, i64) -> () {
-{
+struct FnCallback<'a> {
     name: &'static str,
-    cb: &'a CB,
+    cb: &'a OnClick,
 }
 
 impl<'a> Debug for FnCallback<'a> {
@@ -56,23 +42,21 @@ impl<'a> ClickCallback for FnCallback<'a> {
 }
 
 fn main() {
-    let first = |name, x, y| {
-        println!("1st: name={}, x={}, y={}", name, x, y);
-    };
     let mut b = Button {
         listeners: &mut vec![FnCallback {
             name: "First",
-            cb: &first,
+            cb: &|name, x, y| {
+                println!("1st: name={}, x={}, y={}", name, x, y);
+            },
         }],
     };
     b.click(1, 1);
     b.click(1, 12);
-    let second = |name, x, y| {
-        println!("2nd: name={}, x={}, y={}", name, x, y);
-    };
     b.listen(FnCallback {
         name: "Second",
-        cb: &second,
+        cb: &|name, x, y| {
+            println!("2nd: name={}, x={}, y={}", name, x, y);
+        },
     });
     b.click(2, 2);
     b.click(3, 3);
