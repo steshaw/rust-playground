0c6e6696c4c739e627122ae10af1a259c7e1e353
Steven Shaw <steven@steshaw.org>
2020-01-13 17:19:59 +1000

    refactor

--- crash/examples/03-cli-parse.rs
+++ crash/examples/03-cli-parse.rs
@@ -20,14 +20,16 @@ fn parse_args(args: std::env::Args) -> Result<Frame, ArgsErr> {
         return Err(ArgsErr::TooMany);
     }
     let args = args.collect::<Vec<_>>();
-    if let Ok(width) = args.get(1).unwrap().parse::<u32>() {
-        if let Ok(height) = args.get(2).unwrap().parse::<u32>() {
+    let a1_s = args.get(1).unwrap();
+    let a2_s = args.get(2).unwrap();
+    if let Ok(width) = a1_s.parse::<u32>() {
+        if let Ok(height) = a2_s.parse::<u32>() {
             Ok(Frame { width, height })
         } else {
-            Err(ArgsErr::InvalidInteger(args.get(2).unwrap().to_string()))
+            Err(ArgsErr::InvalidInteger(a2_s.to_string()))
         }
     } else {
-        Err(ArgsErr::InvalidInteger(args.get(1).unwrap().to_string()))
+        Err(ArgsErr::InvalidInteger(a1_s.to_string()))
     }
 }
 
