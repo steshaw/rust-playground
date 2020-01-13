ab310698514dbe5b5d2aadbcd27b22e76b6c5b92
Steven Shaw <steven@steshaw.org>
2020-01-13 17:39:40 +1000

    Less early return

--- crash/examples/03-cli-parse.rs
+++ crash/examples/03-cli-parse.rs
@@ -12,16 +12,39 @@ enum ArgsErr {
 }
 
 fn parse_args(args: std::env::Args) -> Result<Frame, ArgsErr> {
+    fn k(width_s: &str, height_s: &str) -> Result<Frame, ArgsErr> {
+        let width = width_s
+            .parse::<u32>()
+            .or(Err(ArgsErr::InvalidInteger(width_s.to_string())))?;
+        let height = height_s
+            .parse::<u32>()
+            .or(Err(ArgsErr::InvalidInteger(height_s.to_string())))?;
+
+        Ok(Frame { width, height })
+    }
     // FIXME: Early returns and panics galore!
+    let args = args.collect::<Vec<_>>();
+    let a1_s = args.get(1);
+    let a2_s = args.get(2);
+    let a3_s = args.get(3);
+    match (a1_s, a2_s, a3_s) {
+        (None, None, None) => Err(ArgsErr::TooFew),
+        (Some(_), None, None) => Err(ArgsErr::TooFew),
+        (Some(width_s), Some(height_s), None) => k(width_s, height_s),
+        (Some(_), Some(_), Some(_)) => Err(ArgsErr::TooMany),
+        (_, _, _) => {
+            // Other case are impossible!
+            Err(ArgsErr::TooMany)
+        }
+    }
+
+    /*
     if args.len() < 3 {
         return Err(ArgsErr::TooFew);
     }
     if args.len() > 3 {
         return Err(ArgsErr::TooMany);
     }
-    let args = args.collect::<Vec<_>>();
-    let a1_s = args.get(1).unwrap();
-    let a2_s = args.get(2).unwrap();
     if let Ok(width) = a1_s.parse::<u32>() {
         if let Ok(height) = a2_s.parse::<u32>() {
             Ok(Frame { width, height })
@@ -31,6 +54,7 @@ fn parse_args(args: std::env::Args) -> Result<Frame, ArgsErr> {
     } else {
         Err(ArgsErr::InvalidInteger(a1_s.to_string()))
     }
+    */
 }
 
 fn main() {
