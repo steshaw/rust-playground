183fb50344d5e1c458e9a4bd7c6a6d94c3542b28
Steven Shaw <steven@steshaw.org>
2020-01-13 18:46:03 +1000

    Try `ok_or` and `or` with `?`

--- crash/examples/03-cli-parse-nested.rs
+++ crash/examples/03-cli-parse-nested.rs
@@ -17,28 +17,16 @@ fn parse_args(args: std::env::Args) -> Result<Frame, ArgsErr> {
 
     let mut args = args.skip(1);
 
-    let width_s = match args.next() {
-        None => return Err(TooFew),
-        Some(width_s) => width_s,
-    };
-
-    let height_s = match args.next() {
-        None => return Err(TooFew),
-        Some(height_s) => height_s,
-    };
+    let width_s = args.next().ok_or(TooFew)?;
+    let height_s = args.next().ok_or(TooFew)?;
 
     if args.next().is_some() {
         return Err(TooMany);
     };
 
-    let width = match width_s.parse() {
-        Err(_) => return Err(InvalidInteger(width_s)),
-        Ok(width) => width,
-    };
-    let height = match height_s.parse() {
-        Err(_) => return Err(InvalidInteger(height_s)),
-        Ok(height) => height,
-    };
+    let width = width_s.parse().or(Err(InvalidInteger(width_s)))?;
+    let height = height_s.parse().or(Err(InvalidInteger(height_s)))?;
+
     Ok(Frame { width, height })
 }
 
