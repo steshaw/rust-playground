54d6609c24314ec4bab87fdc433ef01424846ca8
Steven Shaw <steven@steshaw.org>
2020-01-13 10:58:55 +1000

    Use for_each

--- crash/examples/03-cli/main.rs
+++ crash/examples/03-cli/main.rs
@@ -33,18 +33,23 @@ fn f4() {
         println!("{:?}", a);
     }
 }
+fn f5() {
+    println!("f5");
+    args().for_each(|a| println!("{:?}", a));
+}
 fn main() {
+    let fns = [f1, f2, f3, f4, f5];
+
     if false {
         let sep_line = || println!();
-        let fns: Vec<fn()> = vec![f1, f2, f3, f4];
+        let fns: Vec<fn()> = fns.to_vec();
         let fns = intersperse_vec(sep_line as fn(), fns);
         for f in fns {
             f();
         }
     }
 
-    let fns = [f1, f2, f3, f4];
-    // FIXME: Can the horrors of this be addressed?
+    // FIXME: Fix the horrors?
     for f in fns.iter().intersperse(&((|| println!()) as fn())) {
         f();
     }
