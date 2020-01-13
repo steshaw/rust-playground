c05bb579aee25deb8d37099c068a95a6f822333b
Steven Shaw <steven@steshaw.org>
2020-01-13 17:55:19 +1000

    rustfmt

--- crash/examples/03-cli-parse.rs
+++ crash/examples/03-cli-parse.rs
@@ -14,8 +14,10 @@ enum ArgsErr {
 #[allow(clippy::or_fun_call)]
 fn parse_args(args: std::env::Args) -> Result<Frame, ArgsErr> {
     fn k(width_s: &str, height_s: &str) -> Result<Frame, ArgsErr> {
-        fn p(u32_s : &str) -> Result<u32, ArgsErr> {
-            u32_s.parse::<u32>().or(Err(ArgsErr::InvalidInteger(u32_s.to_string())))
+        fn p(u32_s: &str) -> Result<u32, ArgsErr> {
+            u32_s
+                .parse::<u32>()
+                .or(Err(ArgsErr::InvalidInteger(u32_s.to_string())))
         }
         let width = p(width_s)?;
         let height = p(height_s)?;
