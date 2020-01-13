0683b6845d977df715c3d80935bbe60c320fdc9e
Steven Shaw <steven@steshaw.org>
2020-01-13 19:22:39 +1000

    method call syntax

--- crash/examples/03-cli-parse-nested.rs
+++ crash/examples/03-cli-parse-nested.rs
@@ -11,22 +11,35 @@ enum ArgsErr {
     InvalidInteger(String),
 }
 
+struct ParseArgs(std::env::Args);
+
+impl ParseArgs {
+    /*
+    fn new() -> ParseArgs {
+        unimplemented!()
+    }
+    */
+
+    fn require_arg(&mut self) -> Result<String, ArgsErr> {
+       self.0.next().ok_or(ArgsErr::TooFew)
+    }
+}
+
 #[allow(clippy::or_fun_call)]
-fn parse_args(mut args: std::env::Args) -> Result<Frame, ArgsErr> {
+fn parse_args(args: std::env::Args) -> Result<Frame, ArgsErr> {
     use self::ArgsErr::*;
 
     //let mut args = args.skip(1);
-
-    let mut require_arg = || args.next().ok_or(TooFew);
+    let mut args = ParseArgs(args);
 
     // Ignore command name.
-    require_arg()?;
+    args.require_arg()?;
 
-    let width_s = require_arg()?;
-    let height_s = require_arg()?;
+    let width_s = args.require_arg()?;
+    let height_s = args.require_arg()?;
 
     // Require end of arguments here.
-    let mut require_no_more_args = || match args.next() {
+    let mut require_no_more_args = || match args.0.next() {
         None => Ok(()),
         Some(_) => Err(TooMany),
     };
