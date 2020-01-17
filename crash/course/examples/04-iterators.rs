65523079766361bfec5e759e2e3ec4ce7ed755ec
Steven Shaw <steven@steshaw.org>
2020-01-17 11:51:42 +1000

    Ex 04-2: infinite answers

--- crash/course/examples/04-iterators.rs
+++ crash/course/examples/04-iterators.rs
@@ -7,9 +7,24 @@ impl Iterator for Empty {
     }
 }
 
+struct UltimateAnswers;
+
+impl Iterator for UltimateAnswers {
+    type Item = u8;
+    fn next(self: &mut Self) -> Option<u8> {
+        Some(42)
+    }
+}
+
 fn main() {
     for _ in Empty {
         panic!("Argh, an element from Empty!");
     }
-    println!("done!");
+    println!("Done with Empty!");
+
+    println!("The answer to the ultimate question of life, the universe and everything:");
+    for answer in UltimateAnswers.take(5) {
+        println!("{}", answer);
+    }
+    println!("Done with UltimateAnswers!");
 }
