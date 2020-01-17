342835be7c5fb58fa6354becfcc1c5766d6893fd
Steven Shaw <steven@steshaw.org>
2020-01-17 12:55:05 +1000

    Complicated doubler

--- crash/course/examples/04-iterators.rs
+++ crash/course/examples/04-iterators.rs
@@ -18,17 +18,20 @@ impl Iterator for UltimateAnswers {
     }
 }
 
-struct Count{current: u32, to: u32}
+struct Count {
+    current: u32,
+    to: u32,
+}
 
 impl Count {
     fn new(to: u32) -> Count {
-        Count{current : 0, to}
+        Count { current: 0, to }
     }
 }
 
 impl Iterator for Count {
     type Item = u32;
-    fn next(& mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<Self::Item> {
         (self.current < self.to).then(|| {
             self.current += 1;
             self.current
@@ -36,44 +39,57 @@ impl Iterator for Count {
     }
 }
 
-struct Fibonacci{state : (u128, u128)}
+struct Fibonacci {
+    state: (u128, u128),
+}
 
 impl Fibonacci {
     fn new() -> Fibonacci {
-        Fibonacci{ state : (0, 1)}
+        Fibonacci { state: (0, 1) }
     }
 }
 
 impl Iterator for Fibonacci {
     type Item = u128;
-    fn next(& mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<Self::Item> {
         let result = Some(self.state.0);
         self.state = (self.state.1, self.state.0 + self.state.1);
         result
     }
 }
 
+struct Doubler<I>(I)
+where
+    I: Iterator<Item = u32>;
+
+impl<I> Iterator for Doubler<I>
+where
+    I: Iterator<Item = u32>,
+{
+    type Item = u32;
+    fn next(&mut self) -> Option<u32> {
+        self.0.next().map(|n| n * 2)
+    }
+}
+
 fn main() {
-    for _ in Empty {
-        panic!("Argh, an element from Empty!");
-    }
-    println!("Done with Empty!");
-    println!();
-
-    println!("The answer to the ultimate question of life, the universe and everything:");
-    for answer in UltimateAnswers.take(5) {
-        println!("{}", answer);
-    }
-    println!("Done with UltimateAnswers!");
+    println!("Empties = {:?}", Empty.collect::<Vec<_>>());
 
     println!();
-    println!("1 to 10");
-    for n in Count::new(10) {
-        println!("{}", n);
-    }
-    println!("Done with Count!");
+    println!(
+        "Ultimate answers = {:?}",
+        UltimateAnswers.take(5).collect::<Vec<_>>()
+    );
+
+    println!();
+    println!("1..10: {:?}", Count::new(10).collect::<Vec<_>>());
 
     println!();
     let fibs = Fibonacci::new().take(10).collect::<Vec<_>>();
     println!("fibs = {:?}", fibs);
+
+    println!();
+    let orig_iter = 1..11;
+    let doubled_iter = Doubler(orig_iter);
+    println!("doubles = {:?}", doubled_iter.collect::<Vec<_>>());
 }
