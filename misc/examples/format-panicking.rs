62cdbce97f9e89b34badf70020f2188c53296625
Steven Shaw <steven@steshaw.org>
2020-01-18 11:58:19 +1000

    Format panicking and no-panic

--- misc/examples/format-panicking.rs
+++ misc/examples/format-panicking.rs
@@ -1,13 +1,24 @@
 use std::fmt;
+use no_panic::no_panic;
 
-struct WriteError;
+struct FormatError;
 
-impl fmt::Display for WriteError {
+impl fmt::Display for FormatError {
     fn fmt(&self, _: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         unimplemented!()
     }
 }
 
+struct FormatNoPanic;
+
+impl fmt::Display for FormatNoPanic {
+    #[no_panic]
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        Ok(())
+    }
+}
+
 fn main() {
-    let _s: String = format!("Here it is: {}", WriteError);
+    println!("Here it is: {}", FormatNoPanic);
+    println!("Here it is: {}", FormatError);
 }
