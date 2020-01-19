d6a30f641f2bce1a7a97ec37f7dd816703f28b84
Steven Shaw <steven@steshaw.org>
2020-01-19 15:43:57 +1000

    More Fn/FnMut/FnOnce examples

--- crash/course/examples/05-move-into-closure.rs
+++ crash/course/examples/05-move-into-closure.rs
@@ -1,23 +1,42 @@
-fn validate_fn<F>(_f: &F)
+// https://www.snoyman.com/blog/2018/11/rust-crash-course-05-rule-of-three
+
+fn call_fn<F>(f: F)
 where
     F: Fn() -> (),
 {
+    f()
 }
-fn validate_fn_mut<F>(_f: &F)
+fn call_fn_mut<F>(mut f: F)
 where
     F: FnMut() -> (),
 {
+    f()
 }
-fn validate_fn_once<F>(_f: &F)
+fn call_fn_once<F>(f: F)
 where
     F: FnOnce() -> (),
 {
+    f()
+}
+
+mod michael_fn_ref {
+    use super::*;
+
+    pub fn main() {
+        let name = "Fred".to_string();
+        let say_hi = || println!("Hello, {}", name);
+        call_fn(say_hi);
+        call_fn_mut(say_hi);
+        call_fn_once(say_hi);
+        say_hi();
+        say_hi();
+    }
 }
 
 mod fn_ref {
     use super::*;
     pub fn main() {
-        let name = String::from("Fred");
+        let name = "Fred".to_string();
 
         let welcome = move || {
             let mut name = name.clone();
@@ -25,15 +44,31 @@ mod fn_ref {
             println!("Welcome, {}", name);
         };
 
-        validate_fn(&welcome);
+        call_fn(&welcome);
 
-        validate_fn_mut(&welcome);
+        call_fn_mut(&welcome);
 
-        validate_fn_once(&welcome);
+        call_fn_once(&welcome);
 
         welcome();
-        //println!("name = {}", name); Compile error: borrow of moved value: `name`.
+        //println!("name = {}", name); // error[E0382]: borrow of moved value: `name`
         welcome();
+    }
+}
+
+mod michael_fn_mut {
+    use super::*;
+    pub fn main() {
+        let mut say_hi = {
+            let mut name = String::from("Alice");
+            move || {
+                name += " and Bob";
+                println!("Hello, {}", name);
+            }
+        };
+        //call_fn(say_hi);
+        call_fn_mut(&mut say_hi);
+        call_fn_once(&mut say_hi);
     }
 }
 
@@ -48,15 +83,58 @@ mod fn_mut {
         };
 
         // error[E0525]: expected a closure that implements the `Fn` trait, but this closure only implements `FnMut`.
-        //validate_fn(&welcome);
+        //call_fn(welcome);
 
-        validate_fn_mut(&welcome);
+        call_fn_mut(&mut welcome);
 
-        validate_fn_once(&welcome);
+        call_fn_once(&mut welcome);
 
         welcome();
-        //println!("name = {}", name); // error[E0382]: borrow of moved value: `name`
         welcome();
+    }
+}
+
+mod michael_fn_once {
+    use super::*;
+    pub fn main() {
+        let say_hi = {
+            let name = String::from("Alice");
+            || {
+                let name = name;
+                println!("Hello, {}", name);
+            }
+        };
+
+        //call_fn(say_hi);
+        //call_fn_mut(say_hi);
+        call_fn_once(say_hi);
+    }
+}
+
+mod michael_fn_once_fixed {
+    use super::*;
+    pub fn main() {
+        let say_hi = {
+            let name = "Alice".to_string();
+            move || println!("Hello, {}", name)
+        };
+
+        call_fn(&say_hi);
+        call_fn_mut(&say_hi);
+        call_fn_once(&say_hi);
+    }
+}
+
+mod michael_fn_once2 {
+    use super::*;
+    pub fn main() {
+        let say_hi = {
+            let name = String::from("Alice");
+            || std::mem::drop(name)
+        };
+        //call_fn(say_hi);
+        //call_fn_mut(say_hi);
+        call_fn_once(say_hi);
     }
 }
 
@@ -72,21 +150,22 @@ mod fn_once {
         };
 
         // error[E0525]: expected a closure that implements the `Fn` trait, but this closure only implements `FnOnce`
-        //validate_fn(&welcome);
+        //call_fn(&welcome);
 
         // error[E0525]: expected a closure that implements the `FnMut` trait, but this closure only implements `FnOnce`
-        //validate_fn_mut(&welcome);
+        //call_fn_mut(&welcome);
 
-        validate_fn_once(&welcome);
-
-        welcome();
-        //println!("name = {}", name); // error[E0382]: borrow of moved value: `name`.
-        //welcome(); // error[E0382]: use of moved value: `welcome`
+        call_fn_once(welcome);
     }
 }
 
 fn main() {
+    michael_fn_ref::main();
     fn_ref::main();
+    michael_fn_mut::main();
     fn_mut::main();
+    michael_fn_once::main();
+    michael_fn_once_fixed::main();
+    michael_fn_once2::main();
     fn_once::main();
 }
