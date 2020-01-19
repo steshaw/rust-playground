04856b9b727adcaa360818245a727358e1289668
Steven Shaw <steven@steshaw.org>
2020-01-19 14:07:20 +1000

    Does not live long enough

--- crash/course/examples/05-closure-ownership.rs
+++ crash/course/examples/05-closure-ownership.rs
@@ -1,17 +1,19 @@
 fn main() {
-    // owned by main
-    let name_outer = String::from("Alice");
+    let say_hi = {
+        // Creation of smaller scope.
+        // Owned by smaller scope.
+        let name_outer = String::from("Alice");
 
-    let say_hi = || {
-        // force a move, again, we'll get smarter in a second
-        let name_inner = name_outer;
-        println!("Hello, {}", name_inner);
+        || {
+            // Use by reference.
+            let name_inner = &name_outer; // Compile error: `name_outer` does not live long enough.
+            println!("Hello, {}", name_inner);
+        }
     };
 
-    // main no longer owns name_outer, try this:
-    //println!("Using name from main: {}", name_outer); // error!
+    // Compile error: cannot find value `name_outer` in this scope.
+    //println!("Using name from main: {}", name_outer);
 
-    // but name_inner lives on, in say_hi!
-    say_hi(); // success
-    //say_hi(); // error!
+    say_hi();
+    say_hi();
 }
