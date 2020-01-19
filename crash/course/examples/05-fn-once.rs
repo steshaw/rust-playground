697812c2df15d101ee889a7e360e5b0df67549a9
Steven Shaw <steven@steshaw.org>
2020-01-19 13:50:09 +1000

    More FnOnce

--- crash/course/examples/05-fn-once.rs
+++ crash/course/examples/05-fn-once.rs
@@ -13,23 +13,56 @@ where
     F: FnOnce() -> (),
 {
     f()
-    //(1..=5).for_each(|_| f() );
 }
 
-fn main() {
+fn once() {
+    println!("\nonce");
     let name = String::from("Alice");
 
     let welcome = || {
-        let mut name = name; // here's the magic
-        name += " and Bob";
+        let name = name; // Move the outer 'name' into the closure.
         println!("Welcome, {}", name);
     };
 
-    //welcome();
-    //println!("before welcome name = {}", name1);
-
-    //call_five_times(welcome);
+    //println!("after welcome name = {}", name); // Compile error: borrow of moved value: `name`.
+    //welcome(); // Compile error at `call_once`: used of moved value: `welcome`.
     call_once(welcome);
+    //println!("after welcome name = {}", name); // Compile error: borrow of moved value: `name`.
+    //welcome(); // Compile error: use of moved value: `welcome`.
+}
 
-    //println!("after welcome name = {}", name1);
+fn five_clone() {
+    println!("\nfive_clone");
+    let name = String::from("Alice");
+
+    let welcome = || {
+        let name = name.clone();
+        println!("Welcome, {}", name);
+    };
+    println!("after welcome name = {}", name);
+    welcome();
+    call_five_times(welcome);
+    println!("after welcome name = {}", name);
+    welcome();
+}
+
+fn five_ref() {
+    println!("\nfive_ref");
+    let name = String::from("Alice");
+
+    let welcome = || {
+        let name = &name;
+        println!("Welcome, {}", name);
+    };
+    println!("after welcome name = {}", name);
+    welcome();
+    call_five_times(welcome);
+    println!("after welcome name = {}", name);
+    welcome();
+}
+
+fn main() {
+    once();
+    five_clone();
+    five_ref();
 }
