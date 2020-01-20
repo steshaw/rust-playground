a6dad592f9093f63aa463f22cb0b2657a2c72024
Steven Shaw <steven@steshaw.org>
2020-01-20 18:31:43 +1000

    Provoke Poison on iteration 3

--- crash/course/examples/05-fearless.rs
+++ crash/course/examples/05-fearless.rs
@@ -3,10 +3,13 @@ use std::sync::Mutex;
 
 fn main() {
     let message = Arc::new(Mutex::new("Fearless".to_string()));
-    for _thread in 1..=10 {
+    for i in 1..=10 {
         let message = message.clone();
         std::thread::spawn(move || {
             let result = message.lock();
+            if i == 3 {
+                panic!("Poison this at {}", i)
+            };
             match result {
                 Err(err) => eprintln!("Poisoned: {}", err),
                 Ok(mut mutex_guard) => {
