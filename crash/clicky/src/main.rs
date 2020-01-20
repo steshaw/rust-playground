e52a5a69181ae182e0c601509f3989849e5fedb9
Steven Shaw <steven@steshaw.org>
2020-01-20 14:59:35 +1000

    Clicky: try the "new way"

--- crash/clicky/src/main.rs
+++ crash/clicky/src/main.rs
@@ -1,13 +1,15 @@
 use gtk::prelude::*;
+use gio::prelude::*;
 
-use gtk::{Button, Window, WindowType};
+use gtk::{Button};
+use gtk::{Window, WindowType};
+use gtk::{Application, ApplicationWindow};
 
-fn main() {
+fn old_way() {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
     }
-
     let window = Window::new(WindowType::Toplevel);
     window.set_title("First GTK+ Program");
     window.set_default_size(350, 70);
@@ -25,4 +27,32 @@ fn main() {
     });
 
     gtk::main();
+}
+
+fn new_way() {
+    let app = Application::new(
+        Some("com.github.gtk-rs.examples.basic"),
+        Default::default(),
+    ).expect("Failed to initialise GTK application");
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("First GTK+ Application");
+        window.set_default_size(350, 70);
+
+        let button = Button::new_with_label("Click me!");
+        window.add(&button);
+        window.show_all();
+    });
+
+    app.run(&[]);
+}
+
+fn main() {
+    let enable_new_way = true;
+    if enable_new_way {
+        new_way();
+    } else {
+        old_way();
+    }
 }
