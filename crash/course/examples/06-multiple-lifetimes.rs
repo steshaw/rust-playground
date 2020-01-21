09c6df2df38fcc3f4324b748fdd8657169a28a56
Steven Shaw <steven@steshaw.org>
2020-01-21 13:29:42 +1000

    Works without multiple lifetimes params

--- crash/course/examples/06-multiple-lifetimes.rs
+++ crash/course/examples/06-multiple-lifetimes.rs
@@ -2,9 +2,12 @@ fn message_and_return<'a>(msg: &String, ret: &'a String) -> &'a String {
     println!("msg = {}", msg);
     ret
 }
+fn foo(name: &String) -> &String {
+    let msg = "C".to_string();
+    message_and_return(&msg, &name)
+}
 fn main() {
     let a = "a".to_string();
-    let b = "b".to_string();
-    let r = message_and_return(&a, &b);
-    assert_eq!("b", r);
+    let r = foo(&a);
+    assert_eq!("a", r);
 }
