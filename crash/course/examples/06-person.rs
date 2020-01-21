3400e9fc7da355813199888d958975b9e2821999
Steven Shaw <steven@steshaw.org>
2020-01-21 12:30:09 +1000

    06-person

---
+++ crash/course/examples/06-person.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+struct Person {
+    name: Option<String>,
+    age: Option<u32>,
+}
+
+fn print_person(person: Person) {
+    match person.name {
+        Some(name) => println!("Name is {}", name),
+        None => println!("No name provided"),
+    }
+
+    match person.age {
+        Some(age) => println!("Age is {}", age),
+        None => println!("No age provided"),
+    }
+}
+
+fn main() {
+    print_person(Person {
+        name: Some(String::from("Alice")),
+        age: Some(30),
+    });
+}
