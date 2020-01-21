865adc958babaca835aa6225458e4210af20e53b
Steven Shaw <steven@steshaw.org>
2020-01-21 12:30:24 +1000

    Printing at top is okay

--- crash/course/examples/06-person.rs
+++ crash/course/examples/06-person.rs
@@ -5,6 +5,7 @@ struct Person {
 }
 
 fn print_person(person: Person) {
+    println!("person = {:?}", person);
     match person.name {
         Some(name) => println!("Name is {}", name),
         None => println!("No name provided"),
