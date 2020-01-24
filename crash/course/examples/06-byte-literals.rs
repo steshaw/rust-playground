b7d916b8e861c690e63a2e016ecccc0b0f9c3472
Steven Shaw <steven@steshaw.org>
2020-01-24 13:39:27 +1000

    06-ex6 Add static lifetime

--- crash/course/examples/06-byte-literals.rs
+++ crash/course/examples/06-byte-literals.rs
@@ -2,11 +2,11 @@ use std::io;
 use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let bytearray1: &[u8; 23] = b"Hello World in binary!\n";
+    let bytearray1: &'static [u8; 23] = b"Hello World in binary!\n";
     println!("{:?}", bytearray1);
     io::stdout().write_all(bytearray1)?;
 
-    let bytearray2: &[u8] = b"Hello World in binary!\n";
+    let bytearray2: &'static [u8] = b"Hello World in binary!\n";
     println!("{:?}", bytearray2);
     io::stdout().write_all(bytearray2)?;
 
