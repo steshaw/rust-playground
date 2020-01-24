72617bddb0a3b4d2dcad065da7c42ce7686fb1fa
Steven Shaw <steven@steshaw.org>
2020-01-24 13:53:16 +1000

    06-str: Ex5

---
+++ crash/course/examples/06-str.rs
@@ -0,0 +1,21 @@
+fn output_arg(arg: &str) {
+    let cs = arg.chars();
+    println!(
+        "arg: {}, characters: {}, bytes: {}",
+        arg,
+        cs.collect::<Vec<_>>().len(),
+        arg.len()
+    );
+}
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+
+    if args.len() == 1 {
+        let default = "שלום";
+        output_arg(default);
+    } else {
+        for a in args {
+            output_arg(&a)
+        }
+    }
+}
