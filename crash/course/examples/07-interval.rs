4884b960b07ca110e8daca9c119dff95d0a50d4d
Steven Shaw <steven@steshaw.org>
2020-01-24 15:11:49 +1000

    More atomic

--- crash/course/examples/07-interval.rs
+++ crash/course/examples/07-interval.rs
@@ -3,12 +3,10 @@ use std::sync::Arc;
 use std::thread::*;
 use std::time::Duration;
 
-// #[derive(Clone)]
 #[derive(Debug)]
 struct Interval {
     counter: Arc<AtomicUsize>,
-    //running: AtomicBool,
-    running: bool,
+    running: Arc<AtomicBool>,
 }
 
 impl Drop for Interval {
@@ -21,19 +19,21 @@ impl Interval {
     fn from_millis(millis: u64) -> Interval {
         let duration = Duration::from_millis(millis);
 
-        //let running = AtomicBool::new(true);
-        let running = true;
+        let running = Arc::new(AtomicBool::new(true));
         let counter = Arc::new(AtomicUsize::new(0));
-        let counter_into_thread = counter.clone();
-        spawn(move || {
-            println!("Interval thread launched!");
 
-            while running {
-                println!("Interval: running = {}", running);
+        let counter_t = counter.clone();
+        let running_t = running.clone();
+
+        spawn(move || {
+            println!("Interval: thread launched!");
+            while running_t.load(Ordering::Relaxed) {
+                println!("Interval: running = {:?}", running_t);
                 sleep(duration);
-                let prev_counter = counter_into_thread.fetch_add(1, Ordering::SeqCst);
+                let prev_counter = counter_t.fetch_add(1, Ordering::SeqCst);
                 println!("prev_counter = {}", prev_counter);
             }
+            println!("Interval: thread ending, running = {:?}", running_t);
         });
 
         Interval { counter, running }
@@ -41,10 +41,10 @@ impl Interval {
 }
 
 fn main() {
-    let mut interval = Interval::from_millis(100);
+    let interval = Interval::from_millis(250);
     sleep(Duration::from_millis(1000));
     println!("Stopping Interval");
-    interval.running = false;
+    interval.running.store(false, Ordering::SeqCst);
     println!("Giving some time for Interval to stop");
     sleep(Duration::from_millis(1000));
 }
