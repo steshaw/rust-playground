843443e94454bd018e6ae741ba1e88ee56fbae05
Steven Shaw <steven@steshaw.org>
2021-10-09 10:31:10 +1000

    Opcode tweaks

--- opcode/opcode.hs
+++ opcode/opcode.hs
@@ -2,18 +2,21 @@ module Main where
 
 import Control.Monad (forM_)
 
-data Opcode = BRK | JSR
-    deriving (Enum, Bounded, Show)
+data Op = BRK | JSR
+  deriving (Enum, Bounded, Show)
 
-data OpcodeInfo = OpcodeInfo {opcode :: Int, size :: Int}
+data Info = Info
+  { code :: Int
+  , size :: Int
+  }
 
-opcodeInfo :: Opcode -> OpcodeInfo
-opcodeInfo BRK = OpcodeInfo 0x00 1
-opcodeInfo JSR = OpcodeInfo 0x20 3
+opcodeInfo :: Op -> Info
+opcodeInfo BRK = Info 0x00 1
+opcodeInfo JSR = Info 0x20 3
 
 main = forM_ [minBound .. maxBound] $ \opcode ->
-    let OpcodeInfo code size = opcodeInfo opcode
-     in putStrLn $
-            show opcode ++ "=" ++ show code
-                ++ " size="
-                ++ show size
+  let i = opcodeInfo opcode
+   in putStrLn $
+        show opcode ++ "=" ++ show (code i)
+          ++ " size="
+          ++ show (size i)
