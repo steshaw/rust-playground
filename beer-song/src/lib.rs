2da23b39c5cdd59ef88a657efb807b2c2880bc9b
Steven Shaw <steven@steshaw.org>
2024-09-06 16:07:08 +1000

    Get programatic in beer song

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,17 +1,26 @@
-pub fn verse(i: i32) -> &'static str {
-    match i {
-		0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
-		1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
-		2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
-		8 => "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n",
-		_ => "Huh?"
+pub fn verse(n: u32) -> String {
+    match n {
+		0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+		1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+		_ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} {} of beer on the wall.\n", n, n, n - 1, if n - 1 > 1 { "bottles"} else { "bottle"} ),
 	}
 }
 
-pub fn sing(i: i32, _j: i32) -> &'static str {
-    match i {
-		8 => "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n",
-		3 => "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
-		_ => "Huh?"
-	}
+pub fn sing(start: u32, end: u32) -> String {
+    println!("start={} end={}", start, end);
+    let mut result = verse(start);
+    let mut n = start - 1;
+    println!("n (init) = {}", n);
+    loop {
+        println!("n (top) = {}", n);
+        result = result + "\n" + &verse(n);
+
+        // Prevent underflow when n == 0.
+        if n == end {
+            break;
+        };
+        n -= 1;
+        println!("n (bot) = {}", n);
+    }
+    result
 }
