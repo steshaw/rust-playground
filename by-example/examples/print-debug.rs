2582ae23910f9ef338d97bdf2ba201b61df9010e
Steven Shaw <steven@steshaw.org>
2024-09-09 15:54:34 +1000

    Print Debug example

---
+++ by-example/examples/print-debug.rs
@@ -0,0 +1,13 @@
+
+// This structure cannot be printed either with `fmt::Display` or
+// with `fmt::Debug`.
+struct UnPrintable(i32);
+
+// The `derive` attribute automatically creates the implementation
+// required to make this `struct` printable with `fmt::Debug`.
+#[derive(Debug)]
+struct DebugPrintable(i32);
+fn main() {
+    println!("DebugPrintable = {:?}", DebugPrintable(42));
+}
+
