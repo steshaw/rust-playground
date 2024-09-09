77a612ed261a91ea46e0ad4fae3b5a40ca70b3e8
Steven Shaw <steven@steshaw.org>
2024-09-09 16:05:12 +1000

    Pretty printing {:#?} and implementing Display

--- by-example/examples/print-debug.rs
+++ by-example/examples/print-debug.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 // This structure cannot be printed either with `fmt::Display` or
 // with `fmt::Debug`.
 struct UnPrintable(i32);
@@ -17,7 +19,7 @@ struct Structure(i32);
 #[derive(Debug)]
 struct Deep(Structure);
 
-fn main() {
+fn debug_example() {
     println!("DebugPrintable = {:?}", DebugPrintable(42));
 
     // Printing with `{:?}` is similar to with `{}`.
@@ -35,4 +37,32 @@ fn main() {
     // The problem with `derive` is there is no control over how
     // the results look. What if I want this to just show a `7`?
     println!("Now {:?} will print!", Deep(Structure(7)));
+}
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+
+impl <'a> Display for Person<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{name} is {age} years old", name = self.name, age=self.age)
+    }
+}
+
+fn person_example() {
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("regular: {}", peter);
+    println!("debug  : {:?}", peter);
+    println!("pretty : {:#?}", peter);
+}
+
+fn main() {
+    debug_example();
+    person_example();
 }
