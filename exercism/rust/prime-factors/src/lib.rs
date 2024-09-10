5a030ec2f07bfc7de519d6c2c4fbe0898a1f5fed
Steven Shaw <steven@steshaw.org>
2024-09-10 16:01:36 +1000

    Tweak

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -2,9 +2,11 @@ pub fn factors(n: u64) -> Vec<u64> {
     let mut m = n;
     let mut primes = vec![];
     let candidate = 2;
-    while m % candidate == 0 {
-        primes.push(candidate);
-        m /= candidate;
+    if m > 1 {
+        while m % candidate == 0 {
+            primes.push(candidate);
+            m /= candidate;
+        }
     }
     if m > 1 {
         primes.push(m);
