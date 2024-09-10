c5deda88f902f1459074b8cad406b99722f36fe0
Steven Shaw <steven@steshaw.org>
2024-09-10 14:40:06 +1000

    Format

--- exercism/rust/armstrong-numbers/src/lib.rs
+++ exercism/rust/armstrong-numbers/src/lib.rs
@@ -1,7 +1,8 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let s = num.to_string();
     let pow = s.len();
-    s.chars().map(|c|
-        c.to_digit(10).unwrap().pow(pow as u32)
-    ).sum::<u32>() == num
+    s.chars()
+        .map(|c| c.to_digit(10).unwrap().pow(pow as u32))
+        .sum::<u32>()
+        == num
 }
