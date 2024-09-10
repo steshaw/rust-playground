058ce86be91ef1697dc06475806f4d84e85d20f6
Steven Shaw <steven@steshaw.org>
2024-09-10 10:26:57 +1000

    Why do we need collect

--- exercism/proverb/src/lib.rs
+++ exercism/proverb/src/lib.rs
@@ -2,7 +2,7 @@ use std::iter::once;
 
 pub fn build_proverb(list: &[&str]) -> String {
     if list.is_empty() {
-        "".to_string()
+        String::new()
     } else {
         list.iter()
             .zip(list.iter().skip(1))
@@ -10,7 +10,7 @@ pub fn build_proverb(list: &[&str]) -> String {
                 format!("For want of a {} the {} was lost.", words.0, words.1)
             })
             .chain(once(format!("And all for the want of a {}.", list[0])))
-            .collect::<Vec<String>>()
+            .collect::<Vec<String>>() // Why do we need collect before join?
             .join("\n")
     }
 }
