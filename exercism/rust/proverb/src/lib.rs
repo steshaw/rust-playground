98fbec19467b2d8672ee68d43349ab0a986a9b3c
Steven Shaw <steven@steshaw.org>
2024-09-10 10:51:47 +1000

    Remove need for testing list is empty

--- exercism/rust/proverb/src/lib.rs
+++ exercism/rust/proverb/src/lib.rs
@@ -1,15 +1,13 @@
-use std::iter::once;
-
 pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        String::new()
-    } else {
-        list.iter()
-            .zip(list.iter().skip(1))
-            .map(|words| {
-                format!("For want of a {} the {} was lost.\n", words.0, words.1)
-            })
-            .chain(once(format!("And all for the want of a {}.", list[0])))
-            .collect()
-    }
+    list.iter()
+        .zip(list.iter().skip(1))
+        .map(|words| {
+            format!("For want of a {} the {} was lost.\n", words.0, words.1)
+        })
+        .chain(
+            list.iter()
+                .take(1)
+                .map(|first| format!("And all for the want of a {}.", first)),
+        )
+        .collect()
 }
