bd1ecee2e5697b9a11df35bad3adf7d1ee3598ed
Steven Shaw <steven@steshaw.org>
2024-09-10 10:22:04 +1000

    Simplify

--- exercism/proverb/src/lib.rs
+++ exercism/proverb/src/lib.rs
@@ -3,21 +3,14 @@ use std::iter::once;
 pub fn build_proverb(list: &[&str]) -> String {
     if list.is_empty() {
         "".to_string()
-    } else if list.len() == 1 {
-        "And all for the want of a ".to_string() + list[0] + "."
-    } else if list.len() == 2 {
-        [
-            "For want of a nail the shoe was lost.",
-            "And all for the want of a nail.",
-        ]
-        .join("\n")
     } else {
-        let r = list
-            .iter()
+        list.iter()
             .zip(list.iter().skip(1))
-            .map(|words| format!("For want of a {} the {} was lost.", words.0, words.1))
+            .map(|words| {
+                format!("For want of a {} the {} was lost.", words.0, words.1)
+            })
+            .chain(once(format!("And all for the want of a {}.", list[0])))
             .collect::<Vec<String>>()
-            .join("\n");
-        format!("{r}\nAnd all for the want of a {}.", list[0])
+            .join("\n")
     }
 }
