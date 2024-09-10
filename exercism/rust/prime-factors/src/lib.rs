d1f83926cc69d1f06221e82c6206067915e623bb
Steven Shaw <steven@steshaw.org>
2024-09-10 16:23:04 +1000

    Rename to result

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn factors(mut n: u64) -> Vec<u64> {
     // Imperative algorithm following Uncle Bob's kata.
-    let mut primes = vec![];
+    let mut result = vec![];
     let mut candidate = 2;
     while n > 1 {
         while n % candidate == 0 {
-            primes.push(candidate);
+            result.push(candidate);
             n /= candidate;
         }
         candidate += 1;
     }
-    primes
+    result
 }
