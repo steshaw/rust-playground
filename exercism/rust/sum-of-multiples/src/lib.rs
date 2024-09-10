c7495767d256c47fe7564bad705e1db18cedff5f
Steven Shaw <steven@steshaw.org>
2024-09-10 18:16:01 +1000

    Filter out zero factors

--- exercism/rust/sum-of-multiples/src/lib.rs
+++ exercism/rust/sum-of-multiples/src/lib.rs
@@ -2,11 +2,9 @@ use std::collections::HashSet;
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples = HashSet::new();
-    for f in factors {
-        if *f > 0 {
-            for x in (*f..limit).step_by(*f as usize) {
-                multiples.insert(x);
-            }
+    for f in factors.iter().filter(|&&f| f > 0) {
+        for x in (*f..limit).step_by(*f as usize) {
+            multiples.insert(x);
         }
     }
     multiples.into_iter().sum()
