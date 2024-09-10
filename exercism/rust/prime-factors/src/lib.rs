a4a0020d2e1333fa79d437242c270c5dc49ed865
Steven Shaw <steven@steshaw.org>
2024-09-10 16:21:26 +1000

    Do not introduce m, just mutate n

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,12 +1,11 @@
-pub fn factors(n: u64) -> Vec<u64> {
+pub fn factors(mut n: u64) -> Vec<u64> {
     // Imperative algorithm following Uncle Bob's kata.
-    let mut m = n;
     let mut primes = vec![];
     let mut candidate = 2;
-    while m > 1 {
-        while m % candidate == 0 {
+    while n > 1 {
+        while n % candidate == 0 {
             primes.push(candidate);
-            m /= candidate;
+            n /= candidate;
         }
         candidate += 1;
     }
