eebb062e9eae0e93473954640e08039656f81a06
Steven Shaw <steven@steshaw.org>
2024-09-10 16:56:34 +1000

    Tidy up

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,12 +1,10 @@
 use std::iter::once;
 
 pub fn factors(mut n: u64) -> Vec<u64> {
-    // Imperative algorithm following Uncle Bob's kata.
     let mut result = Vec::new();
     let mut candidates = once(2).chain((3..).step_by(2));
     while n > 1 {
         let candidate = candidates.next().unwrap();
-        println!("candidate = {}", candidate);
         while n % candidate == 0 {
             result.push(candidate);
             n /= candidate;
