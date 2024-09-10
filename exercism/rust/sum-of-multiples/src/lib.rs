71bcfc8a7efdaa1506c5519c99205b1a38dbc89d
Steven Shaw <steven@steshaw.org>
2024-09-10 18:06:03 +1000

    Try a for loop with a step_by

--- exercism/rust/sum-of-multiples/src/lib.rs
+++ exercism/rust/sum-of-multiples/src/lib.rs
@@ -3,14 +3,9 @@ use itertools::Itertools;
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples = Vec::new();
     for f in factors {
-        println!("factor is {f}");
-        let mut n: u32 = *f;
-        if n > 0 {
-            while n < limit {
-                println!("n is {n}");
-                multiples.push(n);
-
-                n += f;
+        if *f > 0 {
+            for x in (*f..limit).step_by(*f as usize) {
+                multiples.push(x);
             }
         }
     }
