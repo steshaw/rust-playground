a64ede1d2403adaf0cf169f87838f9581eca1e4f
Steven Shaw <steven@steshaw.org>
2024-09-10 10:59:23 +1000

    Try turbofish with _

--- exercism/rust/proverb/src/lib.rs
+++ exercism/rust/proverb/src/lib.rs
@@ -2,12 +2,13 @@ pub fn build_proverb(list: &[&str]) -> String {
     list.iter()
         .zip(list.iter().skip(1))
         .map(|words| {
-            format!("For want of a {} the {} was lost.\n", words.0, words.1)
+            format!("For want of a {} the {} was lost.", words.0, words.1)
         })
         .chain(
             list.iter()
                 .take(1)
                 .map(|first| format!("And all for the want of a {}.", first)),
         )
-        .collect()
+        .collect::<Vec<_>>()
+        .join("\n")
 }
