abffeaec024266da645d421b4a5de82f5b6d0aad
Steven Shaw <steven@steshaw.org>
2024-09-10 15:02:01 +1000

    Calculate grains on chess board

--- exercism/rust/grains/src/lib.rs
+++ exercism/rust/grains/src/lib.rs
@@ -1,7 +1,16 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    if s >= 1 && s <= 64 {
+        let mut num = 1;
+        for _ in 1..s {
+            num = num * 2
+        }
+        num
+    } else {
+        panic!("Invalid square: {s}")
+    }
 }
 
 pub fn total() -> u64 {
-    todo!();
+    // A lot of repetitive calculation here...
+    (1..=64).map(|n| square(n)).sum()
 }
