be3ead0da2ee4d945e90e85e72037caaea544bb3
Steven Shaw <steven@steshaw.org>
2024-09-10 16:42:03 +1000

    Probably Vec::new() is more idiomatic here

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,8 +1,9 @@
 pub fn factors(mut n: u64) -> Vec<u64> {
     // Imperative algorithm following Uncle Bob's kata.
-    let mut result = vec![];
+    let mut result = Vec::new();
     let mut candidate = 2;
     while n > 1 {
+        println!("candidate = {}", candidate);
         while n % candidate == 0 {
             result.push(candidate);
             n /= candidate;
