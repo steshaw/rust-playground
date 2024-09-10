9da2acbc6663d473608b45f25ae5781c08d1e3ac
Steven Shaw <steven@steshaw.org>
2024-09-10 16:53:31 +1000

    Try an iterator for candidates

--- exercism/rust/prime-factors/src/lib.rs
+++ exercism/rust/prime-factors/src/lib.rs
@@ -1,14 +1,14 @@
 pub fn factors(mut n: u64) -> Vec<u64> {
     // Imperative algorithm following Uncle Bob's kata.
     let mut result = Vec::new();
-    let mut candidate = 2;
+    let mut candidates = 2..;
     while n > 1 {
+        let candidate = candidates.next().unwrap();
         println!("candidate = {}", candidate);
         while n % candidate == 0 {
             result.push(candidate);
             n /= candidate;
         }
-        candidate += 1;
     }
     result
 }
