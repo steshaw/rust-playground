7e920ea8f5a53f37b56d74d0276e3427b5e1e93b
Steven Shaw <steven@steshaw.org>
2024-09-10 18:18:59 +1000

    Try for_each instead of for

--- exercism/rust/sum-of-multiples/src/lib.rs
+++ exercism/rust/sum-of-multiples/src/lib.rs
@@ -2,10 +2,10 @@ use std::collections::HashSet;
 
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples = HashSet::new();
-    for f in factors.iter().filter(|&&f| f > 0) {
-        for x in (*f..limit).step_by(*f as usize) {
+    factors.iter().filter(|&&f| f > 0).for_each(|&f| {
+        for x in (f..limit).step_by(f as usize) {
             multiples.insert(x);
         }
-    }
+    });
     multiples.into_iter().sum()
 }
