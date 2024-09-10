4c4c45b102ddbe2c0f83da832277d330b6b982de
Steven Shaw <steven@steshaw.org>
2024-09-10 15:09:10 +1000

    Use 2^square instead of loop

--- exercism/rust/grains/src/lib.rs
+++ exercism/rust/grains/src/lib.rs
@@ -1,16 +1,11 @@
 pub fn square(s: u32) -> u64 {
     if s >= 1 && s <= 64 {
-        let mut num = 1;
-        for _ in 1..s {
-            num = num * 2
-        }
-        num
+        (2 as u64).pow(s - 1)
     } else {
         panic!("Invalid square: {s}")
     }
 }
 
 pub fn total() -> u64 {
-    // A lot of repetitive calculation here...
-    (1..=64).map(|n| square(n)).sum()
+    (1..=64).map(square).sum()
 }
