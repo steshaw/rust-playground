53a115b2a3ecfb2c0d5925ec4c55a1e4a22d292c
Steven Shaw <steven@steshaw.org>
2024-09-11 11:19:57 +1000

    Windows 1255 decoder

---
+++ misc/examples/from-windows-1255.rs
@@ -0,0 +1,31 @@
+#!/usr/bin/env cargo -Zscript
+
+//! ```cargo
+//! [dependencies]
+//! anyhow = "1.0.87"
+//! encoding_rs = "0.8.34"
+//! ```
+//!
+
+// Warning: The "script" functionality above does not work.
+
+//
+// Experimenting with Michael Snoyman's tweet about a quick Window's 1255 decoder.
+// https://x.com/snoyberg/status/1829165444500259015/
+//
+
+use std::io::{BufRead, BufReader};
+
+fn main() -> anyhow::Result<()> {
+    let stdin = std::io::stdin().lock();
+    let stdin = BufReader::new(stdin);
+    for line in stdin.lines() {
+        let line = line?;
+        let line = line.chars()
+            .map(u8::try_from)
+            .collect::<Result<Vec<_>, _>>()?;
+        let (line,_ ,_) = encoding_rs::WINDOWS_1255.decode(&line);
+        println!("{line}");
+    }
+    Ok(())
+}
