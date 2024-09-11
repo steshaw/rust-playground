7c8247306ba5cce03265a423791e77b942d01530
Steven Shaw <steven@steshaw.org>
2024-09-11 14:55:25 +1000

    Pass test 1

--- exercism/rust/matching-brackets/src/lib.rs
+++ exercism/rust/matching-brackets/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    let mut foo = string.chars().filter(|ch|
+        *ch == '[' || *ch == ']' || *ch == '{' || *ch == '}' || *ch == '[' || *ch == ']'
+    ).collect::<String>();
+    println!("foo = {foo:#?}");
+    while foo.len() > 0 {
+        println!("foo = {foo:#?}");
+        let m = match (foo.chars().next().unwrap(), foo.chars().last().unwrap()) {
+            ('[', ']') => true,
+            ('{', '}') => true,
+            ('(', ')') => true,
+            _ => false,
+        };
+        if !m {
+            return false;
+        }
+        // Delete matching pair
+        foo = foo.as_str()[1..foo.len()-1].to_string();
+        println!("foo (updated) = {foo:#?}");
+    }
+    true
 }
