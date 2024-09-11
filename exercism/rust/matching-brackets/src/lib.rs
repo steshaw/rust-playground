4b814507d9a3bcdc3313a476e741665992b36e21
Steven Shaw <steven@steshaw.org>
2024-09-11 16:42:08 +1000

    Try a "parser" style api

--- exercism/rust/matching-brackets/src/lib.rs
+++ exercism/rust/matching-brackets/src/lib.rs
@@ -1,21 +1,41 @@
-use std::collections::HashSet;
+use std::str::Chars;
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let bracket_chars = vec!['[', ']', '{', '}', '(', ')']
-        .into_iter()
-        .collect::<HashSet<_>>();
-    let mut brackets = string.to_string();
-    // Remove all characters but the brackets.
-    brackets.retain(|c| bracket_chars.contains(&c));
-    while brackets.len() > 0 {
-        let l = brackets.len();
-        brackets = brackets
-            .replace("[]", "")
-            .replace("()", "")
-            .replace("{}", "");
-        if brackets.len() == l {
-            break; // Bail if we didn't reduce
+    parse_brackets(&mut string.chars()) == Ok(None)
+}
+
+fn parse_brackets(chars: &mut Chars) -> Result<Option<char>, String> {
+    let c = chars.next();
+    println!("parse_em c = {c:#?}");
+    match c {
+        None => Ok(None),
+        Some('(') => {
+            let r = parse_brackets(chars);
+            match r {
+                Ok(Some(')')) => parse_brackets(chars),
+                _ => Err("Expected right round bracket".to_string()),
+            }
+        }
+        Some('[') => {
+            let r = parse_brackets(chars);
+            match r {
+                Ok(Some(']')) => parse_brackets(chars),
+                _ => Err("Expected right square bracket".to_string()),
+            }
+        }
+        Some('{') => {
+            let r = parse_brackets(chars);
+            match r {
+                Ok(Some('}')) => parse_brackets(chars),
+                _ => Err("Expected right curly bracket".to_string()),
+            }
+        }
+        Some(')') => Ok(c),
+        Some(']') => Ok(c),
+        Some('}') => Ok(c),
+        Some(_) => {
+            // Keep going
+            parse_brackets(chars)
         }
     }
-    brackets.is_empty()
 }
