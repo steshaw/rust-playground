5704970948ab588e1d989ed15f829c2da191c079
Steven Shaw <steven@steshaw.org>
2024-09-11 15:27:44 +1000

    Use a HashSet of bracket characters (and contains)

--- exercism/rust/matching-brackets/src/lib.rs
+++ exercism/rust/matching-brackets/src/lib.rs
@@ -1,15 +1,13 @@
+use std::collections::HashSet;
+
 pub fn brackets_are_balanced(string: &str) -> bool {
+    let bracket_chars = vec!['[', ']', '{', '}', '(', ')']
+        .into_iter()
+        .collect::<HashSet<_>>();
     // Remove all characters but the brackets.
     let mut brackets = string
         .chars()
-        .filter(|ch| {
-            *ch == '['
-                || *ch == ']'
-                || *ch == '{'
-                || *ch == '}'
-                || *ch == '('
-                || *ch == ')'
-        })
+        .filter(|ch| bracket_chars.contains(&ch))
         .collect::<String>();
     while brackets.len() > 0 {
         let l = brackets.len();
