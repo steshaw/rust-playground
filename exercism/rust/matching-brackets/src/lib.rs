79ea560ee39fbc93beeabe7f2db276f413b2b559
Steven Shaw <steven@steshaw.org>
2024-09-12 10:08:04 +1000

    A parsing horror

--- exercism/rust/matching-brackets/src/lib.rs
+++ exercism/rust/matching-brackets/src/lib.rs
@@ -1,41 +1,86 @@
+// This is a horrific experiment. Avert your eyes!
+
+use std::iter::Peekable;
 use std::str::Chars;
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    parse_brackets(&mut string.chars()) == Ok(None)
+    let mut p = string.chars().peekable();
+    println!("peekable = {p:?}");
+    let r = parse_exprs(&mut p);
+    println!("brackets_are_balanced: parse_expr => r = {r:?}");
+    println!("peekable at end = {p:?}");
+    // No parse failures and we are at end-of-input
+    r == Ok(()) && p.next() == None
 }
 
-fn parse_brackets(chars: &mut Chars) -> Result<Option<char>, String> {
-    let c = chars.next();
-    println!("parse_brackets c = {c:#?}");
+fn parse_exprs(chars: &mut Peekable<Chars>) -> Result<(), String> {
+    println!("parse_exprs: {:?}", chars);
+
+    // While we are not at EOS, we can have a sequence of expressions.
+    // {bracket-expressions}
+    loop {
+        let c = chars.peek();
+        if c == None || c == Some(&')') || c == Some(&']') || c == Some(&'}') {
+            break;
+        }
+        let r = parse_expr(chars);
+        if r.is_err() { return r; }
+    }
+
+    Ok(())
+}
+
+fn parse_expr(chars: &mut Peekable<Chars>) -> Result<(), String> {
+    println!("parse_expr: {:?}", chars);
+    let c = chars.peek();
+    println!("parse_expr c = {c:#?}");
     match c {
-        None => Ok(None),
+        None => Ok(()),
         Some('(') => {
-            let r = parse_brackets(chars);
-            match r {
-                Ok(Some(')')) => parse_brackets(chars),
+            chars.next();
+            parse_exprs(chars)?;
+            let c = chars.peek();
+            println!("parse_expr confirming match c = {c:?}");
+            match c {
+                Some(')') => {
+                    chars.next();
+                    Ok(())
+                }
                 _ => Err("Expected right round bracket".to_string()),
             }
         }
         Some('[') => {
-            let r = parse_brackets(chars);
-            match r {
-                Ok(Some(']')) => parse_brackets(chars),
+            chars.next();
+            parse_exprs(chars)?;
+            let c = chars.peek();
+            println!("parse_expr confirming match c = {c:?}");
+            match c {
+                Some(']') => {
+                    chars.next();
+                    Ok(())
+                }
                 _ => Err("Expected right square bracket".to_string()),
             }
         }
         Some('{') => {
-            let r = parse_brackets(chars);
-            match r {
-                Ok(Some('}')) => parse_brackets(chars),
+            chars.next();
+            parse_exprs(chars)?;
+            let c = chars.peek();
+            println!("parse_expr confirming match c = {c:?}");
+            match c {
+                Some('}') => {
+                    chars.next();
+                    Ok(())
+                }
                 _ => Err("Expected right curly bracket".to_string()),
             }
         }
-        Some(')') => Ok(c),
-        Some(']') => Ok(c),
-        Some('}') => Ok(c),
+        Some(')') => Ok(()),
+        Some(']') => Ok(()),
+        Some('}') => Ok(()),
         Some(_) => {
-            // Keep going
-            parse_brackets(chars)
+            chars.next();
+            Ok(())
         }
     }
 }
