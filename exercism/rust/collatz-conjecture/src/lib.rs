f13e69dac047209e6b6ff393b2d718f47a05fbe7
Steven Shaw <steven@steshaw.org>
2024-09-12 18:47:24 +1000

    All tests pass

--- exercism/rust/collatz-conjecture/src/lib.rs
+++ exercism/rust/collatz-conjecture/src/lib.rs
@@ -1,3 +1,12 @@
-pub fn collatz(n: u64) -> Option<u64> {
-    todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n == 0 {
+        None
+    } else {
+        let mut iterations = 0u64;
+        while n != 1 {
+            n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+            iterations += 1;
+        }
+        Some(iterations)
+    }
 }
