c342f25e71955f9293514b383b9f229452a37ad6
Steven Shaw <steven@steshaw.org>
2024-09-13 09:09:21 +1000

    Switch from take(1) to first()

--- exercism/rust/proverb/src/lib.rs
+++ exercism/rust/proverb/src/lib.rs
@@ -6,11 +6,11 @@ pub fn build_proverb(list: &[&str]) -> String {
         .map(|(first, second)| {
             format!("For want of a {first} the {second} was lost.")
         })
-        .chain(
-            list.iter()
-                .take(1)
-                .map(|first| format!("And all for the want of a {}.", first)),
-        )
+        .chain({
+            list.first()
+                .map(|first| format!("And all for the want of a {}.", first))
+                .into_iter()
+        })
         .intersperse("\n".to_string())
         .collect()
 }
