16cb887e32ccfa8f5c75947a2027ae9fa16a5fb2
Steven Shaw <steven@steshaw.org>
2024-09-14 09:27:20 +1000

    Switch to filter

--- exercism/rust/anagram/src/lib.rs
+++ exercism/rust/anagram/src/lib.rs
@@ -4,18 +4,23 @@ pub fn anagrams_for<'a>(
     word: &str,
     possible_anagrams: &[&'a str],
 ) -> HashSet<&'a str> {
-    // For the '{word}' word find anagrams among the following words: {possible_anagrams:?}.
-    let word_lowered = word.to_lowercase().chars().collect::<Vec<_>>();
-    let mut word_sorted = word_lowered.clone();
+    // For the `word`, find anagrams among `possible_anagrams`.
+    let word_lowered = word.to_lowercase();
+    let word_sorted = sorted(&word_lowered);
+    possible_anagrams
+        .iter()
+        .filter(|pa| {
+            let pa_lowered = pa.to_lowercase();
+            let pa_sorted = sorted(&pa_lowered);
+            let anagram_of_self = word_lowered == pa_lowered;
+            !anagram_of_self && word_sorted == pa_sorted
+        })
+        .copied()
+        .collect()
+}
+
+fn sorted(word: &str) -> Vec<char> {
+    let mut word_sorted = word.chars().collect::<Vec<char>>();
     word_sorted.sort();
-    let mut result = HashSet::new();
-    for a in possible_anagrams {
-        let a_lowered = a.to_lowercase().chars().collect::<Vec<_>>();
-        let mut a_sorted = a_lowered.clone();
-        a_sorted.sort();
-        if word_lowered != a_lowered && word_sorted == a_sorted {
-            result.insert(*a);
-        }
-    }
-    result
+    word_sorted
 }
