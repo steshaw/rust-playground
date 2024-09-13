2b0b2d056795ae8200d3cb6e9d970dd3d6eb2254
Steven Shaw <steven@steshaw.org>
2024-09-13 19:48:37 +1000

    Grab Exercism clock exercise — does not even compile!

---
+++ exercism/rust/clock/src/lib.rs
@@ -0,0 +1,11 @@
+pub struct Clock;
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        todo!("Add {minutes} minutes to existing Clock time");
+    }
+}
