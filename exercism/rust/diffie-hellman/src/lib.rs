934520f0f6dd73191e55dfeb9229714bd0e95198
Steven Shaw <steven@steshaw.org>
2024-09-13 17:47:27 +1000

    Pass first test

--- exercism/rust/diffie-hellman/src/lib.rs
+++ exercism/rust/diffie-hellman/src/lib.rs
@@ -1,5 +1,7 @@
+use rand::Rng;
+
 pub fn private_key(p: u64) -> u64 {
-    todo!("Pick a private key greater than 1 and less than {p}")
+    rand::thread_rng().gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
