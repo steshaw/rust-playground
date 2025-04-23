d4ab22457c9c8cf74884928040fa5f47f24de83c
Steven Shaw <steven@steshaw.org>
2025-04-23 17:57:20 +1000

    Try wrapping_add

--- book/examples/ch03-integer-overflow.rs
+++ book/examples/ch03-integer-overflow.rs
@@ -1,9 +1,22 @@
 // This will panic with integer overflow in debug mode, but produce "0"
 // in release mode.
-fn main() {
+fn overflow() {
     let mut i : u8 = 0;
     for _ in 1..=256  {
         i += 1
     }
-    println!("{i}");
+    println!("overflow: {i}");
+}
+// This code uses `wrapping_add` so that it always wraps and never panics
+// (even in debug mode).
+fn wrapping_overflow() {
+    let mut i : u8 = 0;
+    for _ in 1..=256  {
+        i = i.wrapping_add(1)
+    }
+    println!("wrapping overflow: {i}");
+}
+fn main() {
+    wrapping_overflow();
+    overflow();
 }
