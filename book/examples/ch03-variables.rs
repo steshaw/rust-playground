932edc399533fbe4fa14adf2a9de3f8bb151f427
Steven Shaw <steven@steshaw.org>
2025-04-23 15:45:32 +1000

    No distinction between mutable values and assignable variables?

--- book/examples/ch03-variables.rs
+++ book/examples/ch03-variables.rs
@@ -1,6 +1,26 @@
-fn main() {
+fn simple_mut() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+}
+fn mut_as_assignable() {
+    // mut allows the variable to be _reassigned_.
+    let mut s = String::from("hi");
+    println!("The value of s is: {s}");
+    s = String::from("ho");
+    println!("The value of s is reassigned to: {s}");
+}
+fn mut_as_mutable() {
+    // mut allows the value referenced to be mutated.
+    let mut s = String::from("hi");
+    println!("The value of s is: {s}");
+    s.clear();
+    s.push_str("ho");
+    println!("The value of s is changed to: {s}");
+}
+pub fn main() {
+    simple_mut();
+    mut_as_assignable();
+    mut_as_mutable()
 }
