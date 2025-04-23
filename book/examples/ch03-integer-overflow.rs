288693c766480b48a0f0f6dac8fa854b8900ebf6
Steven Shaw <steven@steshaw.org>
2025-04-23 18:04:54 +1000

    Format

--- book/examples/ch03-integer-overflow.rs
+++ book/examples/ch03-integer-overflow.rs
@@ -1,8 +1,8 @@
 // This will panic with integer overflow in debug mode, but produce "0"
 // in release mode.
 fn overflow() {
-    let mut i : u8 = 0;
-    for _ in 1..=256  {
+    let mut i: u8 = 0;
+    for _ in 1..=256 {
         i += 1
     }
     println!("overflow: {i}");
@@ -10,9 +10,9 @@ fn overflow() {
 // This code uses `wrapping_add` so that it always wraps and never panics
 // (even in debug mode).
 fn wrapping_overflow() {
-    let mut i : u8 = 0;
-    for _ in 1..=256  {
-        i = i.wrapping_add(1)
+    let mut i: u8 = 0;
+    for _ in 1..=256 {
+        i = i.wrapping_add(1);
     }
     println!("wrapping overflow: {i}");
 }
