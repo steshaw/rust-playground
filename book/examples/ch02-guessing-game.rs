aebc1839719618f24375bea7c144690c5b400dc5
Steven Shaw <steven@steshaw.org>
2025-04-23 14:28:50 +1000

    Parse and compare guess

--- book/examples/ch02-guessing-game.rs
+++ book/examples/ch02-guessing-game.rs
@@ -1,5 +1,6 @@
-use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -20,5 +21,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+    //let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    match guess.trim().parse::<u32>() {
+        Ok(guess) => {
+            println!("You guessed: {}", guess);
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => println!("You win!"),
+            }
+        },
+        Err(err) => { println!("Please input a number! {err}") },
+    }
 }
